c13bf832a53dfec020fe638a818b8cbf7c1f2145
fernando <luizfernandosr@outlook.com.br>
2025-05-28 20:27:08 -0300

    pt-br index heml | init taks styled

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,6 +1,6 @@
 export default function Header ({ children, ...props }) {
     return (
-        <header className=" p-4 bg-blue-400" {...props} >
+        <header className="p-4 bg-blue-400 text-white" {...props} >
             {children}
         </header>
     )
--- src/Components/Tasks-list/index.jsx
+++ src/Components/Tasks-list/index.jsx
@@ -5,8 +5,11 @@ import Title from "../Title";
 export default function TaskContainer ({}) {
     return(
        <Container className=" border-2 border-b-fuchsia-900 " >
-        <Title>Lista de tarefas</Title>
-        <Container></Container>
+            <Title>Lista de tarefas</Title>
+                <button
+                    className='btn btn-primary w-full'>
+                    Adicionar Tarefa
+                </button>
         <Button>Ver todas</Button>
        </Container>
     )
