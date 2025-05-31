3d7aa89cdc219166a934dd1c5d0660c28249c037
luckfer7 <lucasferreira059@gmail.com>
2025-05-31 16:21:19 -0300

    no meu funciona

--- src/Components/Tasks-list/index.jsx
+++ src/Components/Tasks-list/index.jsx
@@ -4,7 +4,7 @@ import Title from "../Title";
 
 export default function TaskContainer ({}) {
     return(
-       <Container className=" border-2 border-b-fuchsia-900 " >
+       <Container className=" border-2 border-b-fuchsia-900 flex flex-col " >
             <Title>Lista de tarefas</Title>
                 <button
                     className='btn btn-primary w-full'>
