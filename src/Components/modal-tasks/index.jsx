847cad904f9cabe20410421fd5f9c1cd6b5819c4
luckfer7 <lucasferreira059@gmail.com>
2025-06-06 20:21:55 -0300

    quase completo

--- src/Components/modal-tasks/index.jsx
+++ src/Components/modal-tasks/index.jsx
@@ -1,14 +1,51 @@
-export default function ModalTasks({ onClose }) {
+import { useState } from "react";
+import Button from "../Button";
+import Container from "../Container";
+
+export default function ModalTasks({ onClose, onSubmit}) {
+
+    const [ nome, setNome ] = useState('');
+    const [ tempo, setTempo ] = useState(''); 
+
+    const adicionarTarefa = (e) => {
+        e.preventDefault();
+        onSubmit({ nome, tempo })
+        console.log({nome, tempo});
+        
+    }
+
     return (
         <div className="fixed inset-0 backdrop-blur-xs flex justify-center items-center z-50" >
-            <div className="bg-white p-6 rounded-lg shadow-lg w-96" >
+            <div className="bg-blue-200 p-6 rounded-lg shadow-lg w-96" >
                 <h2 className="text-xl font-bold mb-4">Criar nova tarefa</h2>
-                <button
-                    onClick={onClose} // Ao clicar, executa a função que fecha o modal
-                    className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
-                >
-                    Fechar
-                </button>
+
+                <form action="" onSubmit={ adicionarTarefa }>
+                    <div className="flex flex-col" >
+                        <label htmlFor="">Nome da tarefa</label>
+                        <input value={nome} onChange={(e) => setNome(e.target.value)} className=" border-2" type="text" />
+                    </div>
+
+                    <div className="flex flex-col" >
+                        <label htmlFor="">Tempo</label>
+                        <input value={tempo} onChange={(e) => setTempo(e.target.value)} className=" border-2 " type="text" />
+                    </div>
+                    
+                    <Container className=" flex justify-between " >
+                        <Button
+                        onClick={onClose} // Ao clicar, executa a função que fecha o modal
+                        className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
+                        >
+                        Fechar
+                        </Button>
+
+                        <Button
+                            type='submit'
+                            className=" mt-4 bg-blue-400 text-white px-4 py-2 rounded cursor-pointer"
+                        >Salvar tarefa</Button>
+                    </Container>
+
+                </form>
+                
             </div>
         </div>
     )
